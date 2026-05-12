b1805517644ed7af303e6be70a760408eaeddd40
mayukt12 <mayuktsuthar12@gmail.com>
2026-05-12 14:30:55 +0530

    Delete admin/practical_bi.py

--- admin/practical_bi.py
+++
@@ -1,8 +0,0 @@
-import panda as pd
-from sklearn.linear_model import LogisticRegression
-data = pd.read_csv(r'C:/Users/bhard/OneDrive/Book1.csv')
-x= data[['feature1','feature2']]
-y= data[['total']]
-model = Logisticregression(max_iter=1000)
-model.fit(x,y)
-print(model.predict([[75,690]]))
